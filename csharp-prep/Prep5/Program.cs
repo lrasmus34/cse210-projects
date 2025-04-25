b052a681e2cd72fda66cd181eb19ac747620f05c
lrasmus34 <882lhr@gmail.com>
2025-04-25 16:09:20 -0600

    Prep 5 done

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string userName = UserName();
+        float number = Number();
+        float squaredNumber = SquareNumber(number);
+        Result(userName, squaredNumber);
+    }
+
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+
+    static string UserName()
+    {
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+        return name;
+    }
+    static float Number()
+    {
+        Console.Write("What is you favorite number? ");
+        float favNumber = float.Parse(Console.ReadLine());
+        return favNumber;
+    }
+
+    static float SquareNumber(float number)
+    {
+        float square = number * number;
+        return square;
+    }
+
+    static void Result(string name, float square)
+    {
+        Console.WriteLine($"{name}, the square of your number is {square}");
     }
 }
