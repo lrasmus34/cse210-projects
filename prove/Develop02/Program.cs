9e7ea640b87612b404bb806aebd9b0de8dc99fac
lrasmus34 <882lhr@gmail.com>
2025-05-13 19:50:03 -0600

    complete project

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,9 +1,49 @@
 using System;
-
+using System.Collections.Generic;
+using System.IO;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        Journal journal = new Journal();
+        bool running = true;
+
+        while (running)
+        {
+            Console.WriteLine("\nJournal Menu:");
+            Console.WriteLine("1. Write a new entry");
+            Console.WriteLine("2. Display");
+            Console.WriteLine("3. Save");
+            Console.WriteLine("4. Load");
+            Console.WriteLine("5. Exit");
+            Console.Write("Choose an option: ");
+
+            string choice = Console.ReadLine();
+            switch (choice)
+            {
+                case "1":
+                    journal.WriteEntry();
+                    break;
+                case "2":
+                    journal.Display();
+                    break;
+                case "3":
+                    Console.Write("Enter filename to save CSV to: ");
+                    string saveFileName = Console.ReadLine();
+                    journal.SaveToCsv(saveFileName);
+                    break;
+                case "4":
+                    Console.Write("Enter filename to load from: ");
+                    string loadFileName = Console.ReadLine();
+                    journal.LoadFile(loadFileName);
+                    break;
+                case "5":
+                    running = false;
+                    break;
+                default:
+                    Console.WriteLine("Invalid option. Try again.");
+                    break;
+            }
+        }
     }
 }
