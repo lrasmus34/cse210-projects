2de36be865413071ff9e7a758bd06a2092637de7
lrasmus34 <882lhr@gmail.com>
2025-05-23 11:07:25 -0600

    Added comment about stretch in Program class

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,6 +4,7 @@ class Program
 {
     static void Main()
     {
+        //Pulls scriptures from a library of scriptures
         Scripture scripture = ScriptureLibrary.GetRandomScripture();
 
         while (true)
